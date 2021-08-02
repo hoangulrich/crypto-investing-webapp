62c57bdbd2537148950f84e853b2de4d823a6f3a
Hoang Le <artstyle0416@gmail.com>
2021-08-02 18:24:06 +0700

    navbar

---
+++ react-finance-webapp/src/components/Sidebar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SidebarContainer, Icon } from "./SidebarElements";
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+      <Icon></Icon>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
